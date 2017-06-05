62b65ccf7028bb857c4309aefc6f4b1b5905f48b
huailiang.peng <penghuailiang@126.com>
2017-06-05 18:25:59 +0800

    ios support

--- Assets/Plugins/IOS/SystemExtInfo.mm
+++ Assets/Plugins/IOS/SystemExtInfo.mm
@@ -1,5 +1,7 @@
 
 #import <Foundation/Foundation.h>
+#import <CoreTelephony/CTTelephonyNetworkInfo.h>
+#import <CoreTelephony/CTCarrier.h>
 
 
 @interface SystemExtInfo : NSObject
@@ -21,17 +23,48 @@ extern "C"
     	return 2;
     }
 
+    
+    char* MakeStringCopy (const char* string)
+    {
+        if (string == NULL)return NULL;
+        char* res = (char*)malloc(strlen(string) + 1);
+        strcpy(res, string);
+        return res;
+        
+    }
+
+    
+    const char* TransferCode(NSString* pre)
+    {
+        char* str=MakeStringCopy("");
+        if([pre isEqualToString:@"中国移动"])
+        {
+            str=MakeStringCopy("MM");
+        }
+        else if ([pre isEqualToString:@"中国联通"])
+        {
+            str=MakeStringCopy("UN");
+        }
+        else if ([pre isEqualToString:@"中国电信"])
+        {
+            str=MakeStringCopy("DX");
+        }
+        return str;
+    }
+    
     //获取运营商
     const char* CheckSIM()
     {
-    	NSLog(@"CheckSIM---------");
-    	return @"MM";
+        CTTelephonyNetworkInfo *telephonyInfo = [[CTTelephonyNetworkInfo alloc] init];
+        CTCarrier *nsstr = [telephonyInfo subscriberCellularProvider];
+        NSLog(@"CheckSIM  %@",nsstr.carrierName);
+       // return MakeStringCopy([nsstr.carrierName UTF8String]);
+        return TransferCode(nsstr.carrierName);
     }
 
 #if defined (__cplusplus)  
 }
 #endif
-
 
 
 
